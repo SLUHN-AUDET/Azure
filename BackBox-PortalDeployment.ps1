d77a9ad9c44edb635da7ae3532c1b217d6fc5354
SLUHN-AUDET <46357031+SLUHN-AUDET@users.noreply.github.com>
2021-04-07 11:51:21 -0400

    Update BackBox-PortalDeployment.ps1

--- BackBox-PortalDeployment.ps1
+++ BackBox-PortalDeployment.ps1
@@ -14,7 +14,7 @@
     $location= read-host "Please enter location"
     $ErrorActionPreference = 'stop'
     $resourceGroupName = "RG-BackBox"
-    $storageaccountname = "sluhndevbackboxstg" + ( -join (1..100 |Get-Random -Count 6))
+    $storageaccountname = "sluhndevbackboxstg"
     $contname = "$storageaccountname-cont"
     $vhd = Split-Path -Leaf $sourceVHDURI 
    
